c27d8a35275cf126c673e743cdc2c9f3cc495a58
Eric Day <eric.ch.day@gmail.com>
2018-11-23 13:34:33 -0700

    test for one client, function to get one client

--- api_calls.py
+++ api_calls.py
@@ -18,3 +18,20 @@ def getAllClients():
     print(allClients)
 
     return json.dumps(allClients)
+
+
+def getClient(id):
+    allClients = []
+    sql = "SELECT * from clients where id = id"
+    conn = psycopg2.connect('dbname=evolveu')
+    cur = conn.cursor()
+    cur.execute(sql)
+
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    print({'id': row[0], 'name': row[1]})
+
+    return json.dumps([{'id': row[0], 'name': row[1]}])
+
--- test_api_calls.py
+++ test_api_calls.py
@@ -7,5 +7,9 @@ import api_calls
 class TestCreditsCalc(unittest.TestCase):
 
     def test_getAllClients(self):
-        should_be = '[{"id": 1, "name": "Eric Day"}, {"id": 2, "name": "Tish Day"}]'
+        should_be = '[{"id": 1, "name": "John Doe"}, {"id": 2, "name": "Jane Doe"}, {"id": 3, "name": "Next Client"}, {"id": 4, "name": "Previous Client"}, {"id": 5, "name": "Super Client"}]'
         self.assertEqual(should_be, api_calls.getAllClients())
+
+    def test_getClient(self):
+        should_be = '[{"id": 1, "name": "John Doe"}]'
+        self.assertEqual(should_be, api_calls.getClient(1))
